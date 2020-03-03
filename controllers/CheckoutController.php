d1b722ea8df796b1eb6955010577f79802121a45
FurqatMashrabjonov <php_lesson@mail.ru>
2020-03-03 11:54:08 +0000

    products

---
+++ controllers/CheckoutController.php
@@ -0,0 +1,48 @@
+<?php
+namespace CheckoutController;
+
+//require_once './models/Cart.php';
+require_once './vendor/autoload.php';
+
+use CartModel\Cart;
+use SeoModel\Seo;
+
+class CheckoutController {
+
+protected $model_cart;
+protected $model_seo;
+
+public function __construct()
+{
+   $this->model_cart = new Cart;
+   $this->model_seo = new Seo;
+}
+
+public function actionStart()
+{
+   $carts = $this->model_cart->funcStart();
+   $title = $this->model_seo->funcTitle();
+   require_once './views/checkout/view.php';
+}
+
+public function actionRemoveCart()
+{
+   $this->model_cart->funcRemoveCart();
+   header('location:?page=checkout');
+}
+
+public function clearCart()
+{
+   $this->model_cart->funcRemoveCart();
+}
+
+
+public function actionRemoveCartOne($id)
+{
+   $this->model_cart->funcRemoveCartOne($id);
+   header('location:?page=checkout');
+}
+
+}
+
+?>
